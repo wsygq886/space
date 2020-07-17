3c47a9454176f3ed3f904ed41c57b8b623121d09
WWIandMC <1043997450@qq.com>
2020-07-17 13:38:47 +0800

    20200717

--- dsa/循环链表1.c
+++ dsa/循环链表1.c
@@ -1,94 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node{
-	int data;
-	struct Node *next;
-}node;
+void
+input( int *a, int *n );
+
+int
+compare( void const *a, void const *b );
 
 void
-initList( node **l );
-
-void
-createList( node **l, int *a, int n );
-
-void
-listInsert( node **l, int e );
-
-void
-listDelete( node *l );
-
-void
-printList( node *l );
+print( int *a, int n );
 
 int
 main( int argc, char *argv[] )
 {
-	int a[] = { 0, 1, 2, 3, 4};
-	node *l;
-	initList( &l );
-	createList( &l, a, sizeof(a) / sizeof(int));
-	listInsert( &l, 5 );
-	listDelete( l );
+	int n_a, n_b;
+	int a[20], b[20], c[40];
+	input( a, &n_a );
+	input( b, &n_b );
+	qsort( a, sizeof(a), sizeof(a)/sizeof(int), compare );
+	print( a, n_a );
+
 
 	return 0;
 }
 
 void
-initList( node **l )
-{
-	(*l) = ( node* )malloc( sizeof( node ) );
-}
-
-
-void
-createList( node **l, int *a, int n )
+input( int *a, int *n )
 {
 	int i;
-	node *s, *tail = *l;
-
-	for( i = 0; i < n; i++ ){
-		if( i == 0 ){
-			s = *l;
-		}else{
-			s = ( node* )malloc( sizeof( node ) );
-		}
-		s->data = a[i];
-		tail->next = s;
-		tail = s;
+	scanf("%d", n);
+	for( i = 0; i < *n; i++ ){
+		scanf("%d", &a[i]);
 	}
-	tail->next = *l;
-	*l = tail;
+}
+
+int
+compare( void const *a, void const *b )
+{
+	return ( *((int*)a) > *((int*)b) ? 0 : 1 );
 }
 
 void
-listInsert( node **l, int e )
+print( int *a, int n )
 {
-	node *s;
-	s = ( node* )malloc( sizeof( node ) );
-	s->data = e;
-	s->next = (*l)->next;
-	(*l)->next = s;
-	*l = s;
-}
-
-void
-listDelete( node *l )
-{
-	node *t;
-	t = l->next;
-	l->next = l->next->next;
-	free( t );
-}
-
-void
-printList( node *l )
-{
-	node *p = l;
-	if( p != NULL ){
-		do{
-			printf("%d ", p->data );
-			p = p->next;
-		}while( p != l );
+	int i;
+	for( i = 0; i < n; i++ ){
+		printf("%d ", a[i]);
 	}
 }
