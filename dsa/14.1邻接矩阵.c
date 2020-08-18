1a4b896021fc18f912e3a6f4a7d025fa8084b283
WWIandMC <1043997450@qq.com>
2020-08-18 16:31:13 +0800

    2020/08/18

--- dsa/14.1邻接矩阵.c
+++ dsa/14.1邻接矩阵.c
@@ -1,77 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define OK		0
-#define ERROR	1
-
 typedef int  VertexType;
 typedef char ArcType;
 
-typedef struct _arcnode {
-	int adjvex;	//邻接点下标
-	int weight;	//权重
-	struct _arcnode* next;
-}arcnode;
-
-typedef struct _vertexnode {
-	VertexType data;
-	arcnode* head;
-}vertexnode;
-
-typedef struct _Lgraph {
-	vertexnode* arr;
+typedef struct _mgraph {
+	VertexType* vexs;
+	ArcType** arc;
 	int num_vexs;
-}lgraph;
-
-
+}mgraph;
 
 void
-initGraph(lgraph** g);
+initGraph(mgraph** g);
 
 void
-createGraph(lgraph* g);
+createGraph(mgraph* g);
 
 int main()
 {
-	lgraph* g;
+	mgraph* g;
 	initGraph(&g);
 	createGraph(g);
 
 	return 0;
 }
 
-void initGraph(lgraph** g)
-{
-	int n;
-	*g = (lgraph*)malloc(sizeof(lgraph));
-	printf("输入顶点数\n");
-	scanf("%d", &n);
-	(*g)->num_vexs = n;
-	(*g)->arr = (VertexType*)malloc(sizeof(VertexType) * n);
-}
-
-void createGraph(lgraph* g)
+void
+initGraph(mgraph** g)
 {
 	int i;
-	arcnode* p;
-	printf("从0开始按照编号顺序输入顶点值\n");
-	for (i = 0; i < g->num_vexs; i++) {
-		scanf("%d", &g->arr[i].data);
-	}
-	printf("从0开始按照编号顺序输入邻接表, 所以输入数据都不要用空格分开, 输入两个#表示结束\n");
-	for (i = 0; i < g->num_vexs; i++) {
-		char adj, weight;
-		arcnode* tail;
-		p = (arcnode*)malloc(sizeof(arcnode));
-		g->arr[i].head = tail = p;
-		while (adj = getchar() != '#' && weight = getchar() != '#') {
-			p = (arcnode*)malloc(sizeof(arcnode));
-			p->adjvex = adj;
-			p->weight = weight;
-			tail->next = p;
-			tail = p;
-		}
-		tail->next = NULL;
+	*g = (mgraph*)malloc(sizeof(mgraph));
+	printf("输入顶点数\n");
+	scanf("%d", &(*g)->num_vexs);
+
+	(*g)->vexs = (VertexType*)malloc(sizeof(VertexType) * (*g)->num_vexs);
+	(*g)->arc = (ArcType**)malloc(sizeof(ArcType*) * (*g)->num_vexs);
+
+	for (i = 0; i < (*g)->num_vexs; i++) {
+		(*g)->arc[i] = (ArcType*)malloc(sizeof(ArcType) * (*g)->num_vexs);
 	}
 }
 
+
+void
+createGraph(mgraph* g)
+{
+	int i, j;
+	printf("从0开始按照编号顺序输入顶点值\n");
+	for (int i = 0; i < g->num_vexs; i++) {
+		scanf("%d", &g->vexs[i]);
+	}
+	getchar();
+	printf("输入邻接矩阵, 矩阵元素之间不要有空格, 用#代替无穷大\n");
+	for (i = 0; i < g->num_vexs; i++) {
+		for (j = 0; j < g->num_vexs; j++) {
+			scanf("%c", &g->arc[i][j]);
+		}
+		getchar();
+	}
+}
